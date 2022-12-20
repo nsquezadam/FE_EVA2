50bb52e6a8fe76cd3a588d1e1a279e5c1d09ae61
Natalia Quezada <nsquezadam@gmail.com>
2022-12-19 22:06:14 -0300

    cambio

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// calculo de  cotizacion  
+let width  =  document.getElementByID.value;
+let height = document.getElementById.value;
+const priceMtr =  900;
+
+// caculo de los metros cuadrados largo x ancho  
+
+
